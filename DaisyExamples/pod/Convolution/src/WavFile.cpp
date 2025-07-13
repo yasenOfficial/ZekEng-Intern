fc78eeba0aa18384a02c22dd3807ae5e2b0b9f33
yasenOfficial <yasentsv@gmail.com>
2025-07-13 17:42:09 +0300

    from one file to another works in separate func

--- DaisyExamples/pod/Convolution/src/WavFile.cpp
+++ DaisyExamples/pod/Convolution/src/WavFile.cpp
@@ -96,48 +96,122 @@ bool CopyWavFileChunked(const char* infile_name,
                         const char* outfilename,
                         size_t      kChunkSize)
 {
+    FRESULT fr;
     uint8_t header[256];
+    UINT bytesread = 0, byteswritten = 0;
 
-    // Open file
+    // Open input file
     if(f_open(&in_file, infile_name, FA_READ) != FR_OK)
     {
-        hw.PrintLine("Can't Open file");
-        daisy::Logger<daisy::LOGGER_INTERNAL>::Flush();
+        hw.PrintLine("Can't open input file");
         return false;
     }
 
     // Read header
-    UINT bytesread_total = 0;
-    FRESULT fr = f_read(&in_file, header, sizeof(header), &bytesread_total);
-    f_close(&in_file);
-
-    if(fr == FR_OK && bytesread_total == sizeof(header))
+    fr = f_read(&in_file, header, sizeof(header), &bytesread);
+    if(fr != FR_OK || bytesread < 44) // WAV header is at least 44 bytes
     {
-        WavFile wav;
-        if(wav.ParseHeader(header, sizeof(header)))
-        {
-            wav.PrintHeader();
-            // You can access h here if needed
-            const WavFile::Header& h = wav.GetHeader();
-            return true;
-        }
-        else
-        {
-            hw.PrintLine("Not a valid WAV header!");
-            daisy::Logger<daisy::LOGGER_INTERNAL>::Flush();
-            return false;
-        }
-    }
-    else
-    {
-        hw.PrintLine(
-            "Can't read file, f_read returned: %d (read %lu/%lu bytes)",
-            fr,
-            (unsigned long)bytesread_total,
-            (unsigned long)sizeof(header));
-        daisy::Logger<daisy::LOGGER_INTERNAL>::Flush();
+        hw.PrintLine("Can't read WAV header, f_read returned: %d", fr);
+        f_close(&in_file);
         return false;
     }
 
-    return true;
+    // Parse header
+    WavFile wav;
+    if(!wav.ParseHeader(header, bytesread))
+    {
+        hw.PrintLine("Not a valid WAV header!");
+        f_close(&in_file);
+        return false;
+    }
+    wav.PrintHeader();
+    const WavFile::Header& h = wav.GetHeader();
+
+    // Sanity check
+    FILINFO fno;
+    if(f_stat(infile_name, &fno) != FR_OK ||
+       (unsigned long)h.data_offset + (unsigned long)h.data_size > (unsigned long)fno.fsize)
+    {
+        hw.PrintLine("ERROR: data_offset + data_size > filesize! WAV may be corrupt.");
+        f_close(&in_file);
+        return false;
+    }
+
+    // Open output file
+    if(f_open(&out_file, outfilename, FA_WRITE | FA_CREATE_ALWAYS) != FR_OK)
+    {
+        hw.PrintLine("Can't open output file");
+        f_close(&in_file);
+        return false;
+    }
+
+    // Write header (up to data_offset)
+    if(f_lseek(&in_file, 0) != FR_OK)
+    {
+        hw.PrintLine("Failed to seek input file");
+        f_close(&in_file);
+        f_close(&out_file);
+        return false;
+    }
+    UINT headerRead = 0, headerWritten = 0;
+    if(f_read(&in_file, header, h.data_offset, &headerRead) != FR_OK || headerRead != h.data_offset)
+    {
+        hw.PrintLine("Failed to read header from input file");
+        f_close(&in_file);
+        f_close(&out_file);
+        return false;
+    }
+    if(f_write(&out_file, header, h.data_offset, &headerWritten) != FR_OK || headerWritten != h.data_offset)
+    {
+        hw.PrintLine("Failed to write header to output file");
+        f_close(&in_file);
+        f_close(&out_file);
+        return false;
+    }
+
+    // Write audio data chunk by chunk
+    if(f_lseek(&in_file, h.data_offset) != FR_OK)
+    {
+        hw.PrintLine("Failed to seek to data_offset in input file");
+        f_close(&in_file);
+        f_close(&out_file);
+        return false;
+    }
+
+    size_t data_left = h.data_size;
+    uint8_t* chunk = new uint8_t[kChunkSize];
+    while(data_left > 0)
+    {
+        size_t to_read = (data_left > kChunkSize) ? kChunkSize : data_left;
+        bytesread = byteswritten = 0;
+        fr = f_read(&in_file, chunk, to_read, &bytesread);
+        if(fr != FR_OK || bytesread == 0)
+        {
+            hw.PrintLine("Data read failed: res=%d bytes=%lu/%lu", fr, (unsigned long)bytesread, (unsigned long)to_read);
+            break;
+        }
+        fr = f_write(&out_file, chunk, bytesread, &byteswritten);
+        if(fr != FR_OK || byteswritten != bytesread)
+        {
+            hw.PrintLine("Data write failed: res=%d bytes=%lu/%lu", fr, (unsigned long)byteswritten, (unsigned long)bytesread);
+            break;
+        }
+        data_left -= byteswritten;
+        hw.PrintLine("Chunk: wrote %lu bytes, %lu left", (unsigned long)byteswritten, (unsigned long)data_left);
+    }
+    delete[] chunk;
+
+    f_close(&in_file);
+    f_close(&out_file);
+
+    if(data_left == 0)
+    {
+        hw.PrintLine("Wrote WAV file chunked: %s (header: %lu, data: %lu)", outfilename, (unsigned long)h.data_offset, (unsigned long)h.data_size);
+        return true;
+    }
+    else
+    {
+        hw.PrintLine("Failed to write entire WAV file (left: %lu)", (unsigned long)data_left);
+        return false;
+    }
 }
